856506ed81d23e61e4b3ad5c8e570ccfd1a99e67
Emmanuel Fermin <emmanuelfermin1990@gmail.com>
2022-08-24 08:10:57 +0800

    Replaced credit card icon due to incorrect crop

--- components/checkout/NewPaymentForm.js
+++ components/checkout/NewPaymentForm.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
+import classes from "./NewPaymentForm.module.css";
 import * as Yup from "yup";
 import { Formik } from "formik";
 import {
   Box,
   Button,
+  Checkbox,
   FormHelperText,
+  FormGroup,
+  FormControlLabel,
   InputAdornment,
   TextField,
+  InputLabel,
 } from "@mui/material";
 import Image from "next/image";
 import CardIcon from "../../public/static/card-icon.png";
 
 const NewPaymentForm = (props) => {
   const [ccNumber, setCcNumber] = useState("");
+  const [expMonth, setExpMonth] = useState("");
+  const [expYear, setExpYear] = useState("");
 
   const formatAndSetCcNumber = (e) => {
     let val = e.target.value;
@@ -36,19 +43,32 @@ const NewPaymentForm = (props) => {
     }
   };
 
+  const formatAndSetCardExp = (e, type) => {
+    let val = e.target.value;
+    const valArray = val.split(" ").join("").split("");
+
+    if (valArray.length === 3) return;
+    if (isNaN(valArray.join(""))) {
+      return;
+    } else {
+      if (type === "month") setExpMonth(val);
+      if (type === "year") setExpYear(val);
+    }
+  };
+
   return (
     <Formik
       initialValues={{
         cardNum: "",
+        cardExpMonth: "",
+        cardExpYear: "",
         password: "",
         submit: null,
       }}
       validationSchema={Yup.object().shape({
-        cardNum: Yup.number()
-          // .test("len", "Must be exactly 16 characters", (val) => {
-          //   if (val) return val.toString().length === 16;
-          // })
-          .required("Card Number is required"),
+        cardNum: Yup.number().required("Card Number is required"),
+        cardExpMonth: Yup.number().max(31).required(),
+        cardExpYear: Yup.number().max(99).required(),
         password: Yup.string().max(255).required("Password is required"),
       })}
       onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
@@ -62,7 +82,6 @@ const NewPaymentForm = (props) => {
           // if (err.response.status === 422) {
           //   err.message = "Invalid credentials";
           // }
-          console.error("Login JWT Error ", err.message);
           console.error(err);
           // if (mounted.current) {
           //   setStatus({ success: false });
@@ -82,44 +101,112 @@ const NewPaymentForm = (props) => {
         values,
         setFieldValue,
       }) => (
-        <form noValidate onSubmit={handleSubmit} {...props}>
-          <h3>Card Number</h3>
-          <TextField
-            autoComplete="off"
-            error={Boolean(touched.cardNum && errors.cardNum)}
-            fullWidth
-            helperText={touched.cardNum && errors.cardNum}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment
-                  sx={{ pl: "18px", pr: "16px" }}
-                  position="start"
-                >
-                  <Image
-                    src={CardIcon}
-                    alt="Logo of Demo Store"
-                    // width={30}
-                    // height={30}
+        <form
+          noValidate
+          onSubmit={handleSubmit}
+          {...props}
+          className={classes.formControl}
+        >
+          <section className={classes.formGroup}>
+            <div className={classes.column__bordered}>
+              <div className={classes.control}>
+                <InputLabel htmlFor="component-simple">Card number*</InputLabel>
+                <TextField
+                  name="cardNum"
+                  autoComplete="off"
+                  fullWidth
+                  error={Boolean(touched.cardNum && errors.cardNum)}
+                  helperText={touched.cardNum && errors.cardNum}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <Image
+                          src={CardIcon}
+                          alt="Logo of Demo Store"
+                          width={51}
+                          height={30}
+                        />
+                      </InputAdornment>
+                    ),
+                    classes: {
+                      adornedEnd: classes.endAdornment,
+                    },
+                  }}
+                  onBlur={handleBlur}
+                  onChange={(e) => {
+                    handleChange(e);
+                    formatAndSetCcNumber(e);
+                  }}
+                  value={(values.cardNum = ccNumber)}
+                  type="text"
+                  variant="outlined"
+                  size="small"
+                />
+              </div>
+
+              <div className={classes.control}>
+                <InputLabel htmlFor="card-expiration">
+                  {"Valid thru (mm/yy)"}*
+                </InputLabel>
+                <div className={classes.control_children}>
+                  <TextField
+                    name="cardExpMonth"
+                    autoComplete="off"
+                    error={Boolean(touched.cardExpMonth && errors.cardExpMonth)}
+                    // helperText={touched.cardExpMonth && errors.cardExpMonth}
+                    onBlur={handleBlur}
+                    onChange={(e) => {
+                      handleChange(e);
+                      formatAndSetCardExp(e, "month");
+                    }}
+                    value={(values.cardExpMonth = expMonth)}
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    sx={{ width: "50px" }}
+                    aria-describedby="cardExpMonth"
+                    id="cardExpMonth"
                   />
-                </InputAdornment>
-              ),
-            }}
-            margin="normal"
-            name="cardNum"
-            onBlur={handleBlur}
-            onChange={(e) => {
-              handleChange(e);
-              formatAndSetCcNumber(e);
-            }}
-            type="text"
-            value={(values.cardNum = ccNumber)}
-            variant="outlined"
-            size="small"
-          />
+                  {errors.cardExpMonth && (
+                    <FormHelperText
+                      id="cardExpMonth"
+                      className={classes.errorCardExp}
+                    >
+                      Month/Year is required
+                    </FormHelperText>
+                  )}
+                  <span>/</span>
+                  <TextField
+                    name="cardExpYear"
+                    autoComplete="off"
+                    error={Boolean(touched.cardExpYear && errors.cardExpYear)}
+                    onBlur={handleBlur}
+                    onChange={(e) => {
+                      handleChange(e);
+                      formatAndSetCardExp(e, "year");
+                    }}
+                    value={(values.cardExpYear = expYear)}
+                    type="text"
+                    variant="outlined"
+                    size="small"
+                    sx={{ width: "50px" }}
+                  />
+                </div>
+              </div>
+            </div>
+          </section>
           {errors.submit && (
             <FormHelperText error>{errors.submit}</FormHelperText>
           )}
-          <Box>
+
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox />}
+              label="I accept the Terms and Conditions"
+            />
+          </FormGroup>
+
+          <section>
             <Button
               disabled={isSubmitting}
               fullWidth
@@ -128,7 +215,7 @@ const NewPaymentForm = (props) => {
             >
               Place Order
             </Button>
-          </Box>
+          </section>
         </form>
       )}
     </Formik>
