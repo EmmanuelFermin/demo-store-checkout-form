1ac7a9b8e8771096af85739c07ae82acf0bed84c
Emmanuel Fermin <emmanuelfermin1990@gmail.com>
2022-08-25 20:35:22 +0800

    Adjusted styles for Header and Payment Form to near pixel perfect

--- components/checkout/CVVTooltip.js
+++ components/checkout/CVVTooltip.js
@@ -1,17 +1,17 @@
-import React from "react";
+import { Fragment } from "react";
 import Image from "next/image";
 import CVVDisplay from "../../public/static/cvv-location.png";
 
 const CVVTooltip = () => {
   return (
-    <React.Fragment>
+    <Fragment>
       <Image
         src={CVVDisplay}
         alt="CVV/CVC Location"
         // width={500}
         // height={500}
       />
-    </React.Fragment>
+    </Fragment>
   );
 };
 
